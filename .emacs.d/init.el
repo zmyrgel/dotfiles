e8638a92cb0c25c6b2e70bc906ef86d5b9943f84
Timo Myyrä <timo.myyra@gmail.com>
2012-12-02 00:08:11 +0200

    remove old code

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -809,30 +809,12 @@
 ;; External packages
 ;; ------------------------------
 
-;; Add info's to proper place
-(dolist (d (list "emacs-w3m/doc" "bbdb/texinfo" "slime/info"))
-  (let ((dir (expand-file-name (concat-path elisp-dir d))))
-    (when (file-exists-p dir)
-      (add-to-list 'Info-default-directory-list dir))))
-
-;;(autoload 'yas/hippie-try-expand "yasnippet")
-;;(require 'yasnippet)
+(autoload 'yas/hippie-try-expand "yasnippet")
 (eval-after-load "yasnippet"
   '(progn
      (yas-global-mode 1)))
 
 ;; Web Browsing
-(autoload 'w3m "w3m" "Visit the WWW page using w3m" t)
-(autoload 'w3m-find-file "w3m" "Find a local file using emacs-w3m." t)
-(autoload 'w3m-browse-url "w3m" "Ask emacs-w3m to show a URL." t)
-(autoload 'w3m-antenna "w3m-antenna" "Report changes of web sites." t)
-(autoload 'w3m-bookmark-view "w3m-bookmark" "Show bookmarks." t)
-(autoload 'w3m-dtree "w3m-dtree" "Display a directory tree." t)
-(autoload 'w3m-namazu "w3m-namazu" "Search files with Namazu." t)
-(autoload 'w3m-perldoc "w3m-perldoc" "View Perl documents" t)
-(autoload 'w3m-search "w3m-search" "Search words using emacs-w3m." t)
-(autoload 'w3m-weather "w3m-weather" "Display a weather report." t)
-
 (eval-after-load "w3m-search"
   '(progn
      (add-to-list 'w3m-search-engine-alist '("duckduckgo" "https://duckduckgo.com/?q=%s"))
