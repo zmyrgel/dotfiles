b23a0174a55881c25e0aa9dd60f131bbe181368d
Timo Myyrä <timo.myyra@bittivirhe.fi>
2019-04-14 10:38:14 +0300

    zap tide config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1108,27 +1108,6 @@
     (setq c-tab-always-indent nil))
   (add-hook 'ruby-mode-hook 'my/ruby-mode-hook))
 
-;; Javascript
-
-(defun setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1)
-  (company-mode +1))
-
-;; aligns annotation to the right hand side
-(setq company-tooltip-align-annotations t)
-
-;; formats the buffer before saving
-(add-hook 'before-save-hook 'tide-format-before-save)
-
-(add-hook 'typescript-mode-hook #'setup-tide-mode)
-
-;;(setq tide-format-options '(:insertSpaceAfterFunctionKeywordForAnonymousFunctions t :placeOpenBraceOnNewLineForFunctions nil))
-
 ;;; ------------------------------
 ;;; Completion
 ;;; ------------------------------
