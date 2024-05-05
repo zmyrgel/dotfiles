26b669dbd598df531613ad0b67a8bc54d100e4d4
Timo Myyrä <timo.myyra@bittivirhe.fi>
2024-05-05 22:11:53 +0300

    emacs: add systemcrafters org-present and org-roam stuff for testing

--- .config/emacs/init.d/init-org.el
+++ .config/emacs/init.d/init-org.el
@@ -168,6 +168,123 @@
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c c") 'org-capture)
 
+(ensure-packages-present '(visual-fill-column))
+
+(setq visual-fill-column-width 110)
+(setq visual-fill-column-center-text t)
+
+(ensure-packages-present '(org-roam org-present org-ref))
+
+(defun my/org-present-prepare-slide (buffer-name heading)
+  ;; Show only top-level headlines
+  (org-overview)
+
+  ;; Unfold the current entry
+  (org-show-entry)
+
+  ;; Show only direct subheadings of the slide but don't expand them
+  (org-show-children))
+
+(defun my/org-present-start ()
+  ;; Tweak font sizes
+  (setq-local face-remapping-alist '((default (:height 1.5) variable-pitch)
+                                     (header-line (:height 4.0) variable-pitch)
+                                     (org-document-title (:height 1.75) org-document-title)
+                                     (org-code (:height 1.55) org-code)
+                                     (org-verbatim (:height 1.55) org-verbatim)
+                                     (org-block (:height 1.25) org-block)
+                                     (org-block-begin-line (:height 0.7) org-block)))
+
+  ;; Set a blank header line string to create blank space at the top
+  (setq header-line-format " ")
+
+  ;; Display inline images automatically
+  (org-display-inline-images)
+
+  ;; Center the presentation and wrap lines
+  (visual-fill-column-mode 1)
+  (visual-line-mode 1))
+
+(defun my/org-present-end ()
+  ;; Reset font customizations
+  (setq-local face-remapping-alist '((default variable-pitch default)))
+
+  ;; Clear the header line string so that it isn't displayed
+  (setq header-line-format nil)
+
+  ;; Stop displaying inline images
+  (org-remove-inline-images)
+
+  ;; Stop centering the document
+  (visual-fill-column-mode 0)
+  (visual-line-mode 0))
+
+;; fixed-pitch font Jetbrains Mono, ligth
+;; varible-pitch font Iosevka Aile light
+;; colortheme: doom-palenight doom-themes
+
+;; Register hooks with org-present
+(add-hook 'org-present-mode-hook 'my/org-present-start)
+(add-hook 'org-present-mode-quit-hook 'my/org-present-end)
+(add-hook 'org-present-after-navigate-functions 'my/org-present-prepare-slide)
+
+;; Load org-faces to make sure we can set appropriate faces
+(require 'org-faces)
+
+;; Resize Org headings
+(dolist (face '((org-level-1 . 1.2)
+                (org-level-2 . 1.1)
+                (org-level-3 . 1.05)
+                (org-level-4 . 1.0)
+                (org-level-5 . 1.1)
+                (org-level-6 . 1.1)
+                (org-level-7 . 1.1)
+                (org-level-8 . 1.1)))
+  (set-face-attribute (car face) nil :font *my-variable-font* :weight 'medium :height (cdr face)))
+
+;; Make the document title a bit bigger
+(set-face-attribute 'org-document-title nil :font *my-variable-font* :weight 'bold :height 1.3)
+
+;; Make sure certain org faces use the fixed-pitch face when variable-pitch-mode is on
+(set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+(set-face-attribute 'org-table nil :inherit 'fixed-pitch)
+(set-face-attribute 'org-formula nil :inherit 'fixed-pitch)
+(set-face-attribute 'org-code nil :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
+
+
+;; [FREHL] 2024/05/05 10:05:32 INFO: Established secure connection to COFVDCW at 10.0.10.11:22000-10.0.10.1:22000/tcp-client/TLS1.3-TLS_AES_128_GCM_SHA256/LAN-P10-5UCGMOSQB6J5SH4PEPQ9HCO0DM
+;; [FREHL] 2024/05/05 10:05:32 INFO: Device COFVDCW client is "syncthing v1.27.3" named "" at 10.0.10.11:22000-10.0.10.1:22000/tcp-client/TLS1.3-TLS_AES_128_GCM_SHA256/LAN-P10-5UCGMOSQB6J5SH4PEPQ9HCO0DM
+;; [FREHL] 2024/05/05 10:05:33 INFO: Lost primary connection to COFVDCW at 10.0.10.11:22000-10.0.10.1:22000/tcp-client/TLS1.3-TLS_AES_128_GCM_SHA256/LAN-P10-5UCGMOSQB6J5SH4PEPQ9HCO0DM: reading length: EOF (0 remain)
+;; [FREHL] 2024/05/05 10:05:33 INFO: Connection to COFVDCW at 10.0.10.11:22000-10.0.10.1:22000/tcp-client/TLS1.3-TLS_AES_128_GCM_SHA256/LAN-P10-5UCGMOSQB6J5SH4PEPQ9HCO0DM closed: reading length: EOF
+
+
+(setq org-roam-v2-ack t)
+
+(let ((roam-dir "~/Documents/OrgRoamNotes"))
+  (unless (file-directory-p roam-dir)
+    (make-directory roam-dir))
+  (setq org-roam-directory "~/Documents/OrgRoamNotes"))
+
+(setq org-roam-completion-everywhere t)
+
+(define-key org-mode-map (kbd "C-M-i") 'completion-at-point)
+
+(define-key global-map (kbd "C-c n l") 'org-roam-buffer-toggle)
+(define-key global-map (kbd "C-c n f") 'org-roam-node-find)
+(define-key global-map (kbd "C-c n i") 'org-roam-node-insert)
+
+(require 'org-roam-dailies) ;; Ensure the keymap is available
+
+(define-key global-map (kbd "C-c n d") 'org-roam-dailies-map)
+(define-key org-roam-dailies-map "Y" 'org-roam-dailies-capture-yesterday)
+(define-key org-roam-dailies-map "T" 'org-roam-dailies-capture-tomorrow)
+
+(org-roam-db-autosync-mode)
+
 (provide 'init-org)
 
 ;; init-org.el ends here
