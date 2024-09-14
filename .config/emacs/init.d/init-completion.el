ba8dbdeff013c405c4cd0c0c22bf04e476b1e574
Timo Myyrä <timo.myyra@bittivirhe.fi>
2024-09-14 09:58:19 +0300

    emacs: completion stuff

--- .config/emacs/init.d/init-completion.el
+++ .config/emacs/init.d/init-completion.el
@@ -60,42 +60,61 @@
 (setq completion-flex-nospace nil)
 (setq completion-pcm-complete-word-inserts-delimiters nil)
 (setq completion-pcm-word-delimiters "-_./:| ")
+(setq completion-ignore-case t)
+(setq completion-auto-wrap t)
+(setq completion-auto-help 'visible)  ; 'visible 'always 'lazy t nil
+(setq completion-auto-deselect t) ;; check
+;; (setq completion-auto-select 'second-tab) ;; nil t second-tab ;; check
+
 (setq completions-detailed t)
 (setq completions-group t)
 (setq completions-group-sort 'alphabetical)
-(setq minibuffer-prompt-properties
-      '(read-only t cursor-intangible t face minibuffer-prompt))
-(add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
-(setq completion-ignore-case t)
-(setq read-buffer-completion-ignore-case t)
-(setq read-file-name-completion-ignore-case t)
-(setq minibuffer-beginning-of-buffer-movement t)
+(setq completions-sort 'historical)
+(setq completions-header-format #("%s possible completions:
+" 0 25 (face shadow)))
+
 (setq completions-format 'one-column)
 (setq completion-show-help nil)
-(setq minibuffer-eldef-shorten-default t)
+
 (setq echo-keystrokes 0.5)
 (setq read-answer-short t)
+
 (file-name-shadow-mode 1)
 (minibuffer-depth-indicate-mode 1)
 (minibuffer-electric-default-mode 1)
+(minibuffer-regexp-mode 1)
+(setq minibuffer-beginning-of-buffer-movement t)
+(setq minibuffer-eldef-shorten-default t)
+(setq minibuffer-prompt-properties
+      '(read-only t cursor-intangible t face minibuffer-prompt))
 
-(when (version<= "29" emacs-version)
-  (setq completion-auto-wrap t)
-  (setq completions-sort 'alphabetical)
-  (setq completions-header-format #("%s possible completions:
-" 0 25 (face shadow)))
-  (setq completion-auto-help 'visible)
+(setq minibuffer-visible-completions t) ;; check
+
+(add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
 
   ;;; Following should be used when corfu/vertico is not used:
-  (unless (or (featurep 'corfu) (featurep 'vertico))
-    (setq completion-auto-select 'second-tab) ;; nil t second-tab
-    ;; Up/down when completing in the minibuffer
-    (define-key minibuffer-local-map (kbd "C-p") #'minibuffer-previous-completion)
-    (define-key minibuffer-local-map (kbd "C-n") #'minibuffer-next-completion)
-    ;; Up/down when competing in a normal buffer
-    (define-key completion-in-region-mode-map (kbd "C-p") #'minibuffer-previous-completion)
-    (define-key completion-in-region-mode-map (kbd "C-n") #'minibuffer-next-completion))
-)
+(unless (or (featurep 'corfu) (featurep 'vertico))
+
+  ;; previous-line-completion, next-line-completion commands
+
+  ;; Up/down when completing in the minibuffer
+  (define-key minibuffer-local-map (kbd "C-p") #'minibuffer-previous-completion)
+  (define-key minibuffer-local-map (kbd "C-n") #'minibuffer-next-completion)
+  ;; Up/down when competing in a normal buffer
+  (define-key completion-in-region-mode-map (kbd "C-p") #'minibuffer-previous-completion)
+  (define-key completion-in-region-mode-map (kbd "C-n") #'minibuffer-next-completion))
+
+;; icomplete
+
+;; show only icomplete in-buffer display and not *Completions* buffer
+;; (advice-add 'completion-at-point :after #'minibuffer-hide-completions)
+(setq icomplete-in-buffer t)
+
+;; completion preview
+;; (global-completion-preview-mode)
+(setq completion-preview-exact-match-only nil)
+(setq completion-preview-minimum-symbol-length 3)
+(setq completion-preview-idle-delay nil)
 
 ;; imenu: M-g i
 (setq imenu-auto-rescan t)
