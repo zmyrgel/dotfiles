3270bcfc4d27b41ad117df6645c17fbff8ef5357
Timo Myyrä <timo.myyra@bittivirhe.fi>
2026-07-28 23:38:08 +0300

    emacs: init-ai needs with-eval-after-load

--- .config/emacs/init.d/init-ai.el
+++ .config/emacs/init.d/init-ai.el
@@ -9,44 +9,45 @@
 
 (keymap-global-set "C-c ." 'agent-shell)
 
-(when (is-work-laptop-p)
-  (setopt agent-shell-openai-authentication
-          (agent-shell-openai-make-authentication
-           :api-key (lambda ()
-                      (password-lookup :host "api.openai.com"))))
-  (setopt agent-shell-preferred-agent-config
-          (agent-shell-openai-make-codex-config))
-  (setopt agent-shell-clipboard-image-handlers
-          (list
-           (list (cons :command "xclip")
-                 (cons :save (lambda (file-path)
-                               (when-let* ((targets (and (eq (window-system) 'x)
-                                                         (gui-get-selection 'CLIPBOARD 'TARGETS)))
-                                           ((vectorp targets))
-                                           ((not (seq-contains-p targets 'image/png))))
-                                 (error "No image/png in clipboard"))
-                               (with-temp-buffer
-                                 (set-buffer-multibyte nil)
-                                 (let ((exit-code (call-process "xclip" nil t nil
-                                                                "-selection" "clipboard"
-                                                                "-t" "image/png" "-o")))
-                                   (unless (zerop exit-code)
-                                     (error "Command xclip failed with exit code %d" exit-code))
-                                   (write-region (point-min) (point-max) file-path nil 'silent)))))))))
+(with-eval-after-load 'agent-shell
+  (when (is-work-laptop-p)
+    (setopt agent-shell-openai-authentication
+            (agent-shell-openai-make-authentication
+             :api-key (lambda ()
+                        (password-lookup :host "api.openai.com"))))
+    (setopt agent-shell-preferred-agent-config
+            (agent-shell-openai-make-codex-config))
+    (setopt agent-shell-clipboard-image-handlers
+            (list
+             (list (cons :command "xclip")
+                   (cons :save (lambda (file-path)
+                                 (when-let* ((targets (and (eq (window-system) 'x)
+                                                           (gui-get-selection 'CLIPBOARD 'TARGETS)))
+                                             ((vectorp targets))
+                                             ((not (seq-contains-p targets 'image/png))))
+                                   (error "No image/png in clipboard"))
+                                 (with-temp-buffer
+                                   (set-buffer-multibyte nil)
+                                   (let ((exit-code (call-process "xclip" nil t nil
+                                                                  "-selection" "clipboard"
+                                                                  "-t" "image/png" "-o")))
+                                     (unless (zerop exit-code)
+                                       (error "Command xclip failed with exit code %d" exit-code))
+                                     (write-region (point-min) (point-max) file-path nil 'silent)))))))))
 
-(unless (is-work-laptop-p)
-  (setopt agent-shell-mistral-authentication (agent-shell-mistral-make-authentication
-                                              :api-key (lambda ()
-                                                         (auth-source-pick-first-password :host "api-key.mistral.com"))))
-  (setopt agent-shell-preferred-agent-config (agent-shell-mistral-make-config)))
+  (unless (is-work-laptop-p)
+    (setopt agent-shell-mistral-authentication (agent-shell-mistral-make-authentication
+                                                :api-key (lambda ()
+                                                           (auth-source-pick-first-password :host "api-key.mistral.com"))))
+    (setopt agent-shell-preferred-agent-config (agent-shell-mistral-make-config)))
 
-;; Setup MCP servers to share between agents
-(setopt agent-shell-mcp-servers
-        `(((name . "context7")
-           (type . "http")
-           (headers . (((name . "CONTEXT7_API_KEY")
-                        (value . ,(password-lookup :host "mcp.context7.com")))))
-           (url . "https://mcp.context7.com/mcp"))))
+  ;; Setup MCP servers to share between agents
+  (setopt agent-shell-mcp-servers
+          `(((name . "context7")
+             (type . "http")
+             (headers . (((name . "CONTEXT7_API_KEY")
+                          (value . ,(password-lookup :host "mcp.context7.com")))))
+             (url . "https://mcp.context7.com/mcp")))))
 
 (provide 'init-ai)
 
